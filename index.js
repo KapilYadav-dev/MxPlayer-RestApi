d16eda1222c4544dadda0a6d40f1cfc6970bd76d
KapilYadav-dev <kapil1910086@akgec.ac.in>
2021-02-03 00:31:56 +0530

    Removed fix port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.port;
 const fetch = require("node-fetch");
 var rawData = [];
 require("dotenv").config();
